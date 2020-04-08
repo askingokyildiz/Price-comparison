7f8f5460310f5f115fd1e7e496c137bc310e1265
Aşkın Gökyıldız <askingokyildiz@gmail.com>
2020-04-08 04:29:22 +0300

    Price comparison commit

--- test.py
+++ test.py
@@ -3,9 +3,23 @@ from selenium.webdriver.common.keys import Keys
 import threading
 from datetime import datetime, date
 import time
+import pymongo
+from bson.objectid import ObjectId
+import json
 
+#İşlem Süresini Başlatma
 start = time.perf_counter()
 
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["price_comparison"]
+competitorcol = db["competitor"]
+try:
+    for competitor in competitorcol.find():
+        print(competitor["CompetitorTitle"])
+        print(competitor["_id"])
+except:
+    print("Id Bulunamadı.")
 
+#İşlem Süresini Bitirip Yazdırma.
 finish = time.perf_counter()
 print('Bitiş saniyesi:', round(finish-start, 2))
